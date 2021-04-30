e5939377f69f1cb5958b0401ad8332229b23afe3
tanialx <taniathucle@gmail.com>
2021-04-30 11:58:51 +0700

    update board.js: separate checkResolved logic to smaller functions

--- src/board.js
+++ src/board.js
@@ -263,7 +263,7 @@ export default class Board extends React.PureComponent {
             return
         }
         // Else:
-        // A blank square has just been selected but no revious item selection 
+        // A blank square has just been selected but no previous item selection 
         // recorded in board's state
         // Ignore this click event
     }
@@ -280,120 +280,128 @@ export default class Board extends React.PureComponent {
      */
     checkResolved(curr_squares, active_idx_arr) {
         let resolved = []
-        const w = this.w
-        const h = this.h
 
         active_idx_arr.forEach(i => {
-
-            const base_type = curr_squares[i].type
-            const base_color = curr_squares[i].color
-
-            /**
-             * Check horizontal line [ i % w == 0 ] [ i % w == 1 ] ... [ i % w == w-1 ]
-             */
-            {
-                const line_wrap_start = Math.floor(i / h) * w
-                const line_wrap_end = line_wrap_start + w
-                // count forward
-                let countForward = []
-                for (let incr = i + 1; incr < line_wrap_end && curr_squares[incr]
-                    && curr_squares[incr].type === base_type
-                    && curr_squares[incr].color === base_color; incr++) {
-                    countForward.push(incr)
-                }
-                // count backward
-                let countBackward = []
-                for (let decr = i - 1; decr >= line_wrap_start && curr_squares[decr]
-                    && curr_squares[decr].type === base_type
-                    && curr_squares[decr].color === base_color; decr--) {
-                    countBackward.push(decr)
-                }
-                // total:
-                const count = countBackward.length + countForward.length + 1
-                if (count >= 5) {
-                    resolved = [i].concat(countForward, countBackward)
-                }
-            }
-            /**
-             * Check vertical line
-             * [ i - w]
-             * [ i ]
-             * [ i + w]
-             */
-            {
-                // count forward
-                let countForward = []
-                for (let incr = i + w; incr < curr_squares.length && curr_squares[incr]
-                    && curr_squares[incr].type === base_type
-                    && curr_squares[incr].color === base_color; incr += w) {
-                    countForward.push(incr)
-                }
-                // count backward
-                let countBackward = []
-                for (let decr = i - w; decr >= 0 && curr_squares[decr]
-                    && curr_squares[decr].type === base_type
-                    && curr_squares[decr].color === base_color; decr -= w) {
-                    countBackward.push(decr)
-                }
-                // total:
-                const count = countBackward.length + countForward.length + 1
-                if (count >= 5) {
-                    resolved = [i].concat(countForward, countBackward)
-                }
-            }
-
-            /**
-             * Check diagonal line 
-             * DIRECTION: \
-             */
-            {
-                // count forward
-                let countForward = []
-                for (let incr = i + w + 1; incr < curr_squares.length && curr_squares[incr]
-                    && curr_squares[incr].type === base_type
-                    && curr_squares[incr].color === base_color; incr += (w + 1)) {
-                    countForward.push(incr)
-                }
-                // count backward
-                let countBackward = []
-                for (let decr = i - w - 1; decr >= 0 && curr_squares[decr]
-                    && curr_squares[decr].type === base_type
-                    && curr_squares[decr].color === base_color; decr -= (w + 1)) {
-                    countBackward.push(decr)
-                }
-                // total:
-                const count = countBackward.length + countForward.length + 1
-                if (count >= 5) {
-                    resolved = [i].concat(countForward, countBackward)
-                }
-            }
-
-            /**
-            * Check diagonal line 
-            * DIRECTION: /
-            */
-            {
-                // count forward
-                let countForward = []
-                for (let incr = i + w - 1; incr < curr_squares.length && curr_squares[incr]
-                    && curr_squares[incr].type === base_type
-                    && curr_squares[incr].color === base_color; incr += (w - 1)) {
-                    countForward.push(incr)
-                }
-                // count backward
-                let countBackward = []
-                for (let decr = i - w + 1; decr >= 0 && curr_squares[decr]
-                    && curr_squares[decr].type === base_type
-                    && curr_squares[decr].color === base_color; decr -= (w - 1)) {
-                    countBackward.push(decr)
-                }
-                // total:
-                const count = countBackward.length + countForward.length + 1
-                if (count >= 5) {
-                    resolved = [i].concat(countForward, countBackward)
-                }
-            }
+            resolved = resolved
+                .concat(this.checkResolved_horizontal(curr_squares, i))
+                .concat(this.checkResolved_vertical(curr_squares, i, this.w))
+                .concat(this.checkResolved_diagonal(curr_squares, i, this.w))
         })
+        return resolved
+    }
+
+    checkResolved_horizontal(squares, i) {
+
+        /* Check horizontal line [ i % w == 0 ] [ i % w == 1 ] ... [ i % w == w-1 ] */
+
+        let resolved = []
+        const line_wrap = this.lineWrapOfIndex(i)
+
+        // count forward
+        let countForward = []
+        for (let incr = i + 1; incr < line_wrap.end && squares[incr]
+            && squares[incr].type === squares[i].type
+            && squares[incr].color === squares[i].color; incr++) {
+            countForward.push(incr)
+        }
+        // count backward
+        let countBackward = []
+        for (let decr = i - 1; decr >= line_wrap.start && squares[decr]
+            && squares[decr].type === squares[i].type
+            && squares[decr].color === squares[i].color; decr--) {
+            countBackward.push(decr)
+        }
+        // total:
+        const count = countBackward.length + countForward.length + 1
+        if (count >= 5) {
+            resolved = [i].concat(countForward, countBackward)
+        }
+        return resolved
+    }
+
+    checkResolved_vertical(squares, i, w) {
+        /**
+        * Check vertical line
+        * [ i - w]
+        * [ i ]
+        * [ i + w]
+        */
+        let resolved = []
+        // count forward
+        let countForward = []
+        for (let incr = i + w; incr < squares.length && squares[incr]
+            && squares[incr].type === squares[i].type
+            && squares[incr].color === squares[i].color; incr += w) {
+            countForward.push(incr)
+        }
+        // count backward
+        let countBackward = []
+        for (let decr = i - w; decr >= 0 && squares[decr]
+            && squares[decr].type === squares[i].type
+            && squares[decr].color === squares[i].color; decr -= w) {
+            countBackward.push(decr)
+        }
+        // total:
+        const count = countBackward.length + countForward.length + 1
+        if (count >= 5) {
+            resolved = [i].concat(countForward, countBackward)
+        }
+        return resolved
+    }
+
+    checkResolved_diagonal(squares, i, w) {
+        let resolved = []
+        /**
+        * Check diagonal line 
+        * DIRECTION: \
+        */
+        {
+            // count forward
+            let countForward = []
+            for (let incr = i + w + 1; incr < squares.length && squares[incr]
+                && squares[incr].type === squares[i].type
+                && squares[incr].color === squares[i].color; incr += (w + 1)) {
+                countForward.push(incr)
+            }
+            // count backward
+            let countBackward = []
+            for (let decr = i - w - 1; decr >= 0 && squares[decr]
+                && squares[decr].type === squares[i].type
+                && squares[decr].color === squares[i].color; decr -= (w + 1)) {
+                countBackward.push(decr)
+            }
+            // total:
+            const count = countBackward.length + countForward.length + 1
+            if (count >= 5) {
+                resolved = resolved.concat([i], countForward, countBackward)
+            }
+        }
+
+        /**
+        * Check diagonal line 
+        * DIRECTION: /
+        */
+        {
+            // count forward
+            let countForward = []
+            for (let incr = i + w - 1; incr < squares.length && squares[incr]
+                && squares[incr].type === squares[i].type
+                && squares[incr].color === squares[i].color; incr += (w - 1)) {
+                countForward.push(incr)
+            }
+            // count backward
+            let countBackward = []
+            for (let decr = i - w + 1; decr >= 0 && squares[decr]
+                && squares[decr].type === squares[i].type
+                && squares[decr].color === squares[i].color; decr -= (w - 1)) {
+                countBackward.push(decr)
+            }
+            // total:
+            const count = countBackward.length + countForward.length + 1
+            if (count >= 5) {
+                resolved = resolved.concat([i], countForward, countBackward)
+            }
+        }
         return resolved
     }
 
